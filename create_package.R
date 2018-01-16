0688e3c50fafe654cfae14e28cd6665d497b31a6
fvafrCU <fvafrcu@arcor.de>
2018-01-16 08:03:30 +0100

    create package asciidoc

--- create_package.R
+++ create_package.R
@@ -4,11 +4,16 @@ me <- person(given = "Andreas Dominik",
              role = c("aut", "cre"),
              email = "adc-r@arcor.de")
 
-packager::create("../asciidoc")
-packager::set_package_info("../asciidoc",
-                           author_at_r = me,
-                           title = "Create Reports Using `knitr` and `asciidoc`", 
-                           description = "I want to use asciidoc for html and slidy.",
-                           details = "This was inspired by http://kbroman.org/knitr_knutshell/pages/asciidoc.html")
-withr::with_dir("../asciidoc", git)
 
+if (FALSE) {
+    path <- "../asciidoc"
+    packager::create(path)
+    packager::set_package_info(path,
+                               author_at_r = me,
+                               title = "Create Reports Using `knitr` and `asciidoc`", 
+                               description = "I want to use asciidoc for html and slidy.",
+                               details = "This was inspired by http://kbroman.org/knitr_knutshell/pages/asciidoc.html")
+    packager::git_add_commit(path = path, "Update Package Info")
+    file.copy(file.path("..", "funktionen_in_r", "asciidoc.R"), file.path(path, "R"))
+    packager::git_add_commit(path = path, "Add Core Functionality", untracked = TRUE)
+}
